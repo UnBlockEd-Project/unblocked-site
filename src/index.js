093b76b50de2643335c82e269cc75bd21867bb87
Adithya <aramanujam7@gatech.edu>
2021-03-11 14:56:54 -0500

    My test comment
    Creating a vidcast to demonstrate our deployment pipeline and how to add code.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import "antd/dist/antd.css";
 import Router from "./router";
 import i18n from "./translation";
 import * as serviceWorker from "./serviceWorker";
-
-const App = () => (
+// My test comment
+const App= () => (
   <BrowserRouter>
     <I18nextProvider i18n={i18n}>
       <Router />
